47f78437d791cd223879f96ff76ada171124c34e
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2024-04-21 20:31:05 +0200

    Add employee controller

---
+++ employee-service/src/main/java/com/mycompany/employee/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.mycompany.employee;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.saveEmployee(employee));
+    }
+}
