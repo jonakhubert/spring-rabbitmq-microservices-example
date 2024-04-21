be882aadec32530a936af0fc249121d0eedff84d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2024-04-21 16:53:34 +0200

    Add field validations and age field

--- employee-service/src/main/java/com/mycompany/employee/Employee.java
+++ employee-service/src/main/java/com/mycompany/employee/Employee.java
@@ -1,5 +1,6 @@
 package com.mycompany.employee;
 
+import jakarta.validation.constraints.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,17 +9,34 @@ public class Employee {
 
     @Id
     private String id;
+
+    @NotEmpty
+    @NotBlank
     private String firstName;
+
+    @NotEmpty
+    @NotBlank
     private String lastName;
+
+    @Positive
+    @Min(18)
+    @Max(65)
+    private int age;
+
+    @Email
     private String email;
+
+    @NotEmpty
+    @NotBlank
     private String position;
 
     public Employee() {}
 
-    public Employee(String id, String firstName, String lastName, String email, String position) {
+    public Employee(String id, String firstName, String lastName, int age, String email, String position) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
         this.email = email;
         this.position = position;
     }
@@ -27,6 +45,7 @@ public class Employee {
     public String getId() { return id; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
+    public int getAge() { return age; }
     public String getEmail() { return email; }
     public String getPosition() { return position; }
 
@@ -34,6 +53,7 @@ public class Employee {
     public void setId(String id) { this.id = id; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setAge(int age) { this.age = age; }
     public void setEmail(String email) { this.email = email; }
     public void setPosition(String position) { this.position = position; }
 }
