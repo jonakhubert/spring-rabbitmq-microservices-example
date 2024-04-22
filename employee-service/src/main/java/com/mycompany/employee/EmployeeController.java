719b23e49ba82378238aa2994c0b62af0d19aee5
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2024-04-22 20:10:37 +0200

    Add CRUD operations

--- employee-service/src/main/java/com/mycompany/employee/EmployeeController.java
+++ employee-service/src/main/java/com/mycompany/employee/EmployeeController.java
@@ -1,10 +1,10 @@
 package com.mycompany.employee;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/employees")
@@ -17,7 +17,24 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
-        return ResponseEntity.ok(employeeService.saveEmployee(employee));
+    public ResponseEntity<Void> addEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee((employee));
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable("id") String id) {
+        return ResponseEntity.ok(employeeService.getEmployee(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDTO>> getEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable("id") String id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
