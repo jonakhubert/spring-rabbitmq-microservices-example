689dde0913ce34be1bc44ee66443a78944ea22b2
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2024-04-26 23:17:44 +0200

    Add task controller

---
+++ task-service/src/main/java/com/mycompany/task/TaskController.java
@@ -0,0 +1,40 @@
+package com.mycompany.task;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/tasks")
+public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addTask(Task task) {
+        taskService.saveTask(task);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<TaskDTO> getTask(@PathVariable("id") int id) {
+        return ResponseEntity.ok(taskService.getTask(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDTO>> getTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("id") int id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
