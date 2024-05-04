0173a4cb399187dd1abd28494acc0b956757ed0e
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2024-05-04 19:22:35 +0200

    Create task and task detail based on retrieved employee

--- task-service/src/main/java/com/mycompany/task/TaskServiceImpl.java
+++ task-service/src/main/java/com/mycompany/task/TaskServiceImpl.java
@@ -1,7 +1,11 @@
 package com.mycompany.task;
 
+import com.mycompany.employee.EmployeeDTO;
+import com.mycompany.taskdetail.TaskDetail;
+import com.mycompany.taskdetail.TaskDetailService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,10 +15,16 @@ public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
     private final TaskDTOMapper taskDTOMapper;
+    private final RestClient restClient;
+    private final TaskDetailService taskDetailService;
 
-    public TaskServiceImpl(TaskRepository taskRepository, TaskDTOMapper taskDTOMapper) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskDTOMapper taskDTOMapper, RestClient restClient,
+                           TaskDetailService taskDetailService
+    ) {
         this.taskRepository = taskRepository;
         this.taskDTOMapper = taskDTOMapper;
+        this.restClient = restClient;
+        this.taskDetailService = taskDetailService;
     }
 
     @Override
@@ -30,7 +40,28 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void saveTask(Task task) {
-        taskRepository.save(task);
+        var result = restClient.get()
+            .uri("/{id}", task.getAssignee())
+            .retrieve()
+            .toEntity(EmployeeDTO.class);
+
+        var employeeDto = result.getBody();
+
+        if(result.getStatusCode().is2xxSuccessful()) {
+            task.setAssignee(employeeDto.id());
+            taskRepository.save(task);
+
+            var taskDetail = new TaskDetail();
+            taskDetail.setEmployeeId(employeeDto.id());
+            taskDetail.setEmployeeFirstName(employeeDto.firstName());
+            taskDetail.setEmployeeLastName(employeeDto.lastName());
+            taskDetail.setTaskTitle(task.getTitle());
+            taskDetail.setTaskDescription(task.getDescription());
+            taskDetail.setTaskStatus(task.getStatus());
+            taskDetail.setTaskPriority(task.getPriority());
+
+            taskDetailService.saveTaskDetail(taskDetail);
+        }
     }
 
     @Override
